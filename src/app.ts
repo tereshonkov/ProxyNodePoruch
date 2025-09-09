5f2da49034354f3a0c3f468deaf443acfbfb45cb
tereshonkov <tereshonkov.dima@gmail.com>
2025-09-09 16:14:38 +0300

    Refactor express middleware usage for improved readability

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,12 @@ import cookieParser from 'cookie-parser';
 
 
 const app = express();
-
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: ['http://localhost:3000', 'https://poruch.vercel.app'],
     credentials: true,
 }));
-app.use(express.json());
+
 
 export default app;
